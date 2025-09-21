c3035a65e8756b8f75a37cc3b1742694011a5086
Rongmario <rongmario@proton.me>
2025-09-21 12:55:27 +0100

    Remove redundant things in the coremod

--- src/main/java/org/spongepowered/mod/SpongeCoremod.java
+++ src/main/java/org/spongepowered/mod/SpongeCoremod.java
@@ -25,15 +25,12 @@
 package org.spongepowered.mod;
 
 import net.minecraft.launchwrapper.Launch;
-import net.minecraftforge.common.ForgeVersion;
-import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.relauncher.FMLInjectionData;
 import net.minecraftforge.fml.relauncher.FMLLaunchHandler;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
 import org.spongepowered.asm.mixin.MixinEnvironment;
 import org.spongepowered.asm.mixin.MixinEnvironment.Phase;
 import org.spongepowered.asm.mixin.Mixins;
-import org.spongepowered.asm.mixin.extensibility.IEnvironmentTokenProvider;
 import org.spongepowered.common.launch.SpongeLaunch;
 import zone.rong.mixinbooter.IEarlyMixinLoader;
 
@@ -45,47 +42,19 @@ public class SpongeCoremod implements IFMLLoadingPlugin, IEarlyMixinLoader {
 
     static File modFile;
 
-    public static final class TokenProvider implements IEnvironmentTokenProvider {
-
-        @Override
-        public int getPriority() {
-            return IEnvironmentTokenProvider.DEFAULT_PRIORITY;
-        }
-
-        @Override
-        public Integer getToken(String token, MixinEnvironment env) {
-            if ("FORGE".equals(token)) {
-                return Integer.valueOf(ForgeVersion.getBuildVersion());
-            } else if ("FML".equals(token)) {
-                String fmlVersion = Loader.instance().getFMLVersionString();
-                int build = Integer.parseInt(fmlVersion.substring(fmlVersion.lastIndexOf('.') + 1));
-                return Integer.valueOf(build);
-            }
-            return null;
-        }
-
-    }
-
     public SpongeCoremod() {
-        SpongeLaunch.addJreExtensionsToClassPath();
+//        SpongeLaunch.addJreExtensionsToClassPath();
 
-        Launch.classLoader.addClassLoaderExclusion("org.spongepowered.common.launch.");
         Launch.classLoader.addClassLoaderExclusion("org.slf4j.");
 
         // Let's get this party started
         SpongeLaunch.initPaths((File) FMLInjectionData.data()[6]); // 6 = game dir
-        SpongeLaunch.setupMixinEnvironment();
+//        SpongeLaunch.setupMixinEnvironment();
 
         // Detect dev/production env
         if (this.isProductionEnvironment()) {
             Mixins.registerErrorHandlerClass("org.spongepowered.mod.mixin.handler.MixinErrorHandler");
         }
-
-        MixinEnvironment.getDefaultEnvironment().registerTokenProviderClass("org.spongepowered.mod.SpongeCoremod$TokenProvider");
-
-        // Add pre-init mixins
-        MixinEnvironment.getEnvironment(Phase.PREINIT).registerTokenProviderClass("org.spongepowered.mod.SpongeCoremod$TokenProvider");
-        MixinEnvironment.getEnvironment(Phase.INIT).registerTokenProviderClass("org.spongepowered.mod.SpongeCoremod$TokenProvider");
 
         Launch.classLoader.addClassLoaderExclusion("org.spongepowered.api.event.Cancellable");
         Launch.classLoader.addClassLoaderExclusion("org.spongepowered.api.eventgencore.annotation.PropertySettings");
